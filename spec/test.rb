60e216fcaf5b7364b0f640369bc269dc8f010e8d
ashraffares <ashraffares090@gmail.com>
2021-02-26 12:12:45 +0200

    adding job test

--- spec/test.rb
+++ spec/test.rb
@@ -36,4 +36,11 @@ describe Logic do
     end
   end
 
+  describe '#job' do
+    it 'it should return Array' do
+      a = logic.job('https://wuzzuf.net/search/jobs/?q=full+stack+web+devdloper')
+      expect(a.class).to eql Array
+    end
+  end
+
 end
