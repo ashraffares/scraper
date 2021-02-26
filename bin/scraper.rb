b56a784087d1fad762e1255f9b195e65e49e2413
ashraffares <ashraffares090@gmail.com>
2021-02-26 12:05:56 +0200

    adding job_scrape display job content

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -16,4 +16,24 @@ class Main < Logic
           puts '________________________________________________________________________________________________________________________'
         end
       end
+
+      def job_scrape
+        puts 'Search jobs: '
+        sub_url = gets.chomp
+        url = job_url(sub_url)
+        begin
+          job(url).each_with_index do |q, index|
+            puts index
+            puts q.joblink.to_s
+            puts '---------------------------------------------------------------------------------------------'
+            puts "#{q.title} |  #{q.location} | #{q.porf} | #{q.posttime}"
+            puts '--------------------------------------------------------------------------------------------'
+            puts "#{q.description} "
+            puts '________________________________________________________________________________________________________________________'
+          end
+        rescue NoMethodError
+          puts 'invalid inputs try again'.red
+          nil
+        end
+      end
 end
